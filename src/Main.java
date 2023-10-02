558ac074861fe6d45bd4effccb549b7768e1d6ab
Liz Correa <acorrear001@alumno.uaemex.mx>
2023-10-02 07:53:52 -0600

    Added new class

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("soy esme!");
+        System.out.println("Y yo Adrian");
+        System.out.println("Y yo Carmen");
+
+        double num1;
+        double num2;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ingresa un numero: ");
+        num1 = in.nextDouble();
+        System.out.println("Ingresa un segundo numero: ");
+        num2 = in.nextDouble();
+
+        Sum sum = new Sum(num1, num2);
+        double result = sum.calculate();
+
+        System.out.println("La suma es: " + result);
+
+    }
+}
