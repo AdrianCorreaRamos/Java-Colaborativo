8e9c05cb3bff8caf4fbaf6e495a55558ffb09d39
AurelioChico <aromerom019@alumno.uaemex.mx>
2023-10-02 08:13:25 -0600

    Actuualizacion

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        System.out.println("soy esme!");
+        System.out.println("soy Aurelio");
         System.out.println("Y yo Adrian");
-        System.out.println("Y yo Carmen");
+
 
         double num1;
         double num2;
