753200d03cf0d4b516b68669bcb01b8bec378c16
Liz Correa <acorrear001@alumno.uaemex.mx>
2023-10-02 08:57:57 -0600

    Actualización main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        System.out.println("soy esme!");
-        System.out.println("Y yo Adrian");
-        System.out.println("Y yo Carmen");
 
         double num1;
         double num2;
